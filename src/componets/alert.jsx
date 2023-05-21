8a544df34d314090718589c9e0aec0ee4ea25de9
keygecat <alyamk@list.ru>
2023-05-22 01:00:48 +0300

    third commit

--- src/componets/alert.jsx
+++ src/componets/alert.jsx
@@ -5,11 +5,13 @@ function Alert(props) {
     const {name = "", closeAlert = Function.prototype} = props;
 
     useEffect(() => {
+        
         const timerId = setTimeout(closeAlert, 3000);
 
         return () => {
             clearTimeout(timerId);
         };
+
     }, [name]);
 
     return (
